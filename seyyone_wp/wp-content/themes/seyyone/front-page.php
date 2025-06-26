30318bf26fb26e4415f15967d0bb67a4de316458
Ranjith <d.ranjithmca@gmail.com>
2025-06-26 10:37:35 +0530

    init wp

--- seyyone_wp/wp-content/themes/seyyone/front-page.php
+++ seyyone_wp/wp-content/themes/seyyone/front-page.php
@@ -691,3 +691,22 @@ $banner_url = get_field('home_banner_image');
 
 <?php get_footer(); ?>
 
+<style>
+    /* custom css */
+.underlined-text {
+    position: relative;
+    color: #3f5cd6;
+    font-weight: bold;
+}
+
+.underlined-text:after {
+    content: "";
+    position: absolute;
+    bottom: -5px;
+    left: 0;
+    width: 100%;
+    height: 4px;
+    background: #3f5cd6;
+}
+
+    </style>
