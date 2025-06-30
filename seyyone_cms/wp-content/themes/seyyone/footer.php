34539bbef5bf2c47f77d35fa642d157f780acbe9
Ranjith <d.ranjithmca@gmail.com>
2025-06-30 17:16:33 +0530

    added

--- seyyone_cms/wp-content/themes/seyyone/footer.php
+++ seyyone_cms/wp-content/themes/seyyone/footer.php
@@ -6,8 +6,7 @@
                 <div class="logo-area">
                    
                     <p class="disc mt-4">
-                        
-                        <span style="color: #3498db;"><strong>Seyyone<strong></span> is a leading provider of Healthcare KPO and Software Solutions with over 25 years of experience serving clients worldwide.
+                        <span style="color: #3498db;"><b>Seyyone</b></span> is a leading provider of Healthcare KPO and Software Solutions with over 25 years of experience serving clients worldwide.
                     </p>
                     <div class="contact-info mt-3">
                         <div class="icon">
